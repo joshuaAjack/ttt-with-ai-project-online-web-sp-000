ed415f0c16744e62db0f3c676c84fed6165ba324
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-03-19 03:53:22 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -1,92 +1,135 @@
 class Game
-  attr_accessor :board, :player_1, :player_2, :timer
+  attr_accessor :player_1, :player_2, :board
 
   WIN_COMBINATIONS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [6, 4, 2]
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
   ]
 
-  def initialize(player_1 = Player::Human.new("X"), player_2 = Player::Human.new("O"), board = Board.new, wargame = false)
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new())
     @player_1 = player_1
     @player_2 = player_2
     @board = board
-    @wargame = wargame
-    @timer = 1.5
   end
 
+  # def current_player
+  #   if @board.turn_count % 2 == 0
+  #     player_1
+  #   else
+  #     player_2
+  #   end
+  # end
+
   def current_player
-    board.turn_count.even?  ? player_1 : player_2
+    @board.turn_count % 2 == 0 ? player_1 : player_2
   end
+
+  # def won?
+  #   WIN_COMBINATIONS.each do |win_combination|
+  #
+  #       win_index_1 = win_combination[0]
+  #       win_index_2 = win_combination[1]
+  #       win_index_3 = win_combination[2]
+  #
+  #       position_1 = board.cells[win_index_1]
+  #       position_2 = board.cells[win_index_2]
+  #       position_3 = board.cells[win_index_3]
+  #
+  #         if position_1 == "X" && position_2 == "X" && position_3 == "X"
+  #             return win_combination
+  #          elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+  #             return win_combination
+  #         end
+  #     end
+  #     false
+  # end
+
   def won?
-    WIN_COMBINATIONS.find do |combo|
-      board.cells[combo[0]] == board.cells[combo[1]] && board.cells[combo[1]] == board.cells[combo[2]] && board.cells[combo[0]] != " "
+    WIN_COMBINATIONS.find do |winner|
+      @board.cells[winner[0]] == @board.cells[winner[1]] &&
+      @board.cells[winner[1]] == @board.cells[winner[2]] &&
+      (@board.cells[winner[0]] == "X" || @board.cells[winner[0]] == "O")
     end
   end
+
+
+  # def draw?
+  #     if board.full? == false
+  #       false
+  #     elsif won? == false
+  #       true
+  #     end
+  # end
+
   def draw?
     board.full? && !won?
   end
+
+
+  # def over?
+  #   if draw?
+  #     true
+  #   elsif won?
+  #     true
+  #   else false
+  #   end
+  # end
+
   def over?
-    won? || draw?
+    draw? || won?
   end
+
+  # def winner
+  #   WIN_COMBINATIONS.each do |win_combination|
+  #
+  #     win_index_1 = win_combination[0]
+  #     win_index_2 = win_combination[1]
+  #     win_index_3 = win_combination[2]
+  #
+  #     position_1 = board.cells[win_index_1]
+  #     position_2 = board.cells[win_index_2]
+  #     position_3 = board.cells[win_index_3]
+  #
+  #     if position_1 == "X" && position_2 == "X" && position_3 == "X"
+  #       return "X"
+  #     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+  #       return "O"
+  #     end
+  #   end
+  #   return nil
+  # end
+
   def winner
-    board.cells[won?[0]] if won?
-  end
-  def turn
-    puts "It's now #{current_player.token}'s turn."
-    input = current_player.move(board, timer).to_i
-    if board.valid_move?(input.to_s)
-      board.update(input, current_player)
-      system('clear')
-      puts "Game #{@counter}" if @wargame
-      board.display
-    elsif input.between?(1, 9) == false
-      puts "That is an invalid move"
-      turn
-    else
-      puts "Whoops! Looks like that position is taken"
-      turn
+    if wincombo = won?
+      @winner = @board.cells[wincombo[0]]
     end
   end
+
+  def turn
+    input = current_player.move(@board)
+    if @board.valid_move?(input)
+      @board.update(input, current_player)
+    else
+      turn
+    end
+    board.display()
+  end
+
   def play
-    board.reset!
-    system('clear')
-    puts "Game #{@counter}" if @wargame
-    board.display
     until over?
       turn
     end
-    if draw?
-      puts "WINNER: NONE"
-    elsif won?
-      puts "WINNER: #{winner}"
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cat's Game!"
     end
   end
-  def wargames
-    @counter = 0
-    x = 0
-    o = 0
-    draw = 0
-    until @counter == 100
-      @counter += 1
-      play
-      if draw?
-        draw += 1
-      elsif winner == "X"
-        x += 1
-      elsif winner == "O"
-        o += 1
-      end
-      sleep(@timer*1.5)
-      @timer -= (@timer/3)
-    end
-    puts "This round had #{x} wins for X, #{o} wins for O, and #{draw} draws."
-    puts "A STRANGE GAME. THE ONLY WINNING MOVE IS NOT TO PLAY."
-    puts "HOW ABOUT A NICE GAME OF CHESS?"
-  end
+
 end
