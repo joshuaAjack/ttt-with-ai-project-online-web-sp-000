bc1db41614a023e956f3909d05fe285f90ed5152
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-03-19 03:48:56 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,38 +1,45 @@
 class Board
-  attr_accessor :cells
+  attr_accessor :cells, :player
+
   def initialize
     reset!
   end
 
   def reset!
-    @cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    @cells = Array.new(9, " ")
   end
+
   def display
-    puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
-    puts "-----------"
-    puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
-    puts "-----------"
-    puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
+      puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+      puts "-----------"
+      puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
+      puts "-----------"
+      puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
   end
+
   def position(input)
-    input = input.to_i
-    cells[input - 1]
+      cells[input.to_i - 1]
   end
+
   def full?
-    cells.none? {|cell| cell == " " || cell == nil}
+    cells.all? { |cell| cell != " "  }
   end
+
+  
   def turn_count
-    cells.count {|cell| cell == "X" || cell == "O"}
+    cells.count { |char| char == "X" || char == "O" }
   end
+
   def taken?(input)
-    position(input) == "X" || position(input) == "O"
+    position(input) != " "
   end
+
   def valid_move?(input)
-    input = input.to_i
-    !(taken?(input)) && input.between?(1, 9)
+    input.to_i.between?(1, 9) && !taken?(input)
   end
+
   def update(input, player)
-    input = input.to_i
-    cells[input -1] = player.token
+    cells[input.to_i - 1] = player.token
+    
   end
 end
